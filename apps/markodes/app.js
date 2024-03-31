3057ff847f9a983f5e201962971f9753b64d63e2
Mark Dochstader <mark.dochstader@absorblms.com>
2024-03-31 08:22:47 -0600

    stub for app

---
+++ apps/markodes/app.js
@@ -0,0 +1,15 @@
+var barcode;
+
+// read settings file, or if it doesn't exist use {}
+var settings = require('Storage').readJSON("markodes.json", true) || {};
+var barcode = settings.code1 || 'NO CODE'; // default
+delete settings; // remove settings from memory
+
+
+
+g.clear();
+g.setFontAlign(0,0);
+g.setFont("Vector",80);
+g.drawString(barcode, 120, 120);
+Bangle.setLCDPower(1);
+
